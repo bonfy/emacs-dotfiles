1da84a8c27f64e6e8732d90c8ef480ae69a2346a
bonfy <foreverbonfy@163.com>
2018-08-08 21:08:04 +0800

    add melpa use-package and which-key

--- init.el
+++ init.el
@@ -1,3 +1,21 @@
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+;; install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
