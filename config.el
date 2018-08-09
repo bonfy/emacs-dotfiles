40fb5aa37e25d540d7301656e0fc6a6a5a611187
bonfy <foreverbonfy@163.com>
2018-08-09 15:40:28 +0800

    add switch-window rainbow & avy

--- config.el
+++ config.el
@@ -29,6 +29,32 @@
 
 (setq ring-bell-function 'ignore)
 
+(use-package switch-window
+  :ensure t
+  :config
+    (setq switch-window-input-style 'minibuffer)
+    (setq switch-window-increase 4)
+    (setq switch-window-threshold 2)
+    (setq switch-window-shortcut-style 'qwerty)
+    (setq switch-window-qwerty-shortcuts
+        '("a" "s" "d" "f" "j" "k" "l" "i" "o"))
+  :bind
+    ([remap other-window] . switch-window))
+
+(defun split-and-follow-horizontally ()
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
+
+(defun split-and-follow-vertically ()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
+
 ;; mode set
 (when window-system (global-hl-line-mode t))
 (global-prettify-symbols-mode t)
@@ -38,6 +64,11 @@
 (setq ido-create-new-buffer 'always)
 (setq ido-everywhere t)
 (ido-mode 1)
+
+(use-package rainbow-mode
+  :ensure t
+  :init
+    (add-hook 'prog-mode-hook 'rainbow-mode))
 
 ;; install spacemacs-theme
 (unless (package-installed-p 'spacemacs-theme)
@@ -58,7 +89,22 @@
   :init
   (beacon-mode 1))
 
+(use-package avy
+  :ensure t
+  :bind
+  ("M-s" . avy-goto-char))
+
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode))))
+
+(defun config-visit ()
+  (interactive)
+  (find-file "~/.emacs.d/config.org"))
+(global-set-key (kbd "C-c e") 'config-visit)
+
+(defun config-reload()
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(global-set-key (kbd "C-c r") 'config-reload)
