cbe4ff5c4f97e4f1c6cbcf98fd286004350b8977
bonfy <foreverbonfy@163.com>
2018-08-09 11:02:54 +0800

    add beacon and kbd

--- README.md
+++ README.md
@@ -13,5 +13,7 @@ Emacs Dotfiles
 * `M-x customize`     自定义界面设置
 * `M-x customize-themes`
 * `M-x list-packages`
+* `M-x term`          调出原生 Terminal
+* `M-x ansi-term`     这个用的更多 更快，而且可以用 emacs 快捷键
 
 * [https://www.emacswiki.org/](https://www.emacswiki.org/)
--- init.el
+++ init.el
@@ -26,11 +26,46 @@
 ;; Install Packages End
 
 
+;; Use Package Define
 
 (use-package which-key
   :ensure t
   :init
   (which-key-mode))
+
+
+(use-package beacon
+  :ensure t
+  :init
+  (beacon-mode 1))
+
+;; Use Package End
+
+
+;; yes-or-no change to y-or-n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Default Ansi Shell
+
+;; (defvar my-fish-shell "/usr/local/bin/fish")
+;; (defvar my-zsh-shell "/usr/local/bin/zsh")
+;; (defadvice ansi-term (before force-bash)
+;;   (interactive (list my-fish-shell)))
+;; (ad-activate 'ansi-term)
+
+(global-set-key (kbd "<s-return>") 'ansi-term)
+
+(setq scroll-conservatively 100)
+
+(setq ring-bell-function 'ignore)
+
+;; mode set
+(when window-system (global-hl-line-mode t))
+(global-prettify-symbols-mode t)
+
+;; backupfile setting
+;; (setq make-backup-files nil)
+;; (setq auto-save-default nil)
 
 ;; hide tool-bar
 (tool-bar-mode -1)
@@ -53,7 +88,7 @@
  '(custom-safe-themes
    (quote
     ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (spacemacs-theme which-key use-package))))
+ '(package-selected-packages (quote (beacon spacemacs-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
