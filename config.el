be8e959bc3b65dcd71d3e60e83ad8c2139991284
bonfy <foreverbonfy@163.com>
2018-08-09 13:49:32 +0800

    use org-babel-load-file to orgnize settings

---
+++ config.el
@@ -0,0 +1,53 @@
+;; hide startup frame message
+(setq inhibit-startup-message t)
+
+;; hide tool-bar
+(tool-bar-mode -1)
+
+;; hide menu-bar
+(menu-bar-mode -1)
+
+;; hide scroll-bar
+(scroll-bar-mode -1)
+
+;; backupfile setting
+;; (setq make-backup-files nil)
+;; (setq auto-save-default nil)
+
+;; yes-or-no change to y-or-n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; (defvar my-fish-shell "/usr/local/bin/fish")
+;; (defvar my-zsh-shell "/usr/local/bin/zsh")
+;; (defadvice ansi-term (before force-bash)
+;;   (interactive (list my-fish-shell)))
+;; (ad-activate 'ansi-term)
+
+(global-set-key (kbd "<s-return>") 'ansi-term)
+
+(setq scroll-conservatively 100)
+
+(setq ring-bell-function 'ignore)
+
+;; mode set
+(when window-system (global-hl-line-mode t))
+(global-prettify-symbols-mode t)
+
+;; install spacemacs-theme
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+(unless (package-installed-p 'dracula-theme)
+  (package-refresh-contents)
+  (package-install 'dracula-theme))
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+(use-package beacon
+  :ensure t
+  :init
+  (beacon-mode 1))
--- init.el
+++ init.el
@@ -1,81 +1,29 @@
+;; Bonfy's Emacs Config
+
 (require 'package)
 
 
 (setq package-enable-at-startup nil)
-
-;; hide startup frame message
-(setq inhibit-startup-message t)
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
-;; Install Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; install use-package
+;; bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; install spacemacs-theme
-(unless (package-installed-p 'spacemacs-theme)
-  (package-refresh-contents)
-  (package-install 'spacemacs-theme))
-
-;; Install Packages End
+;; This is the actual config file.
+;; It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 
-;; Use Package Define
-
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode))
-
-
-(use-package beacon
-  :ensure t
-  :init
-  (beacon-mode 1))
-
-;; Use Package End
-
-
-;; yes-or-no change to y-or-n
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Default Ansi Shell
-
-;; (defvar my-fish-shell "/usr/local/bin/fish")
-;; (defvar my-zsh-shell "/usr/local/bin/zsh")
-;; (defadvice ansi-term (before force-bash)
-;;   (interactive (list my-fish-shell)))
-;; (ad-activate 'ansi-term)
-
-(global-set-key (kbd "<s-return>") 'ansi-term)
-
-(setq scroll-conservatively 100)
-
-(setq ring-bell-function 'ignore)
-
-;; mode set
-(when window-system (global-hl-line-mode t))
-(global-prettify-symbols-mode t)
-
-;; backupfile setting
-;; (setq make-backup-files nil)
-;; (setq auto-save-default nil)
-
-;; hide tool-bar
-(tool-bar-mode -1)
-
-;; hide menu-bar
-(menu-bar-mode -1)
-
-;; hide scroll-bar
-(scroll-bar-mode -1)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -88,7 +36,9 @@
  '(custom-safe-themes
    (quote
     ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (beacon spacemacs-theme which-key use-package))))
+ '(package-selected-packages
+   (quote
+    (dracula-theme beacon spacemacs-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
